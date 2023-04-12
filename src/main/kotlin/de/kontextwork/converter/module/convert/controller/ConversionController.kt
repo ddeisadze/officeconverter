f5c5fac1fc35d17881f811bc8add5dbd4e4151a5
David Deisadze <tbilisidavid@hotmail.com>
2023-04-12 14:54:40 -0400

    Cross origin *

--- src/main/kotlin/de/kontextwork/converter/module/convert/controller/ConversionController.kt
+++ src/main/kotlin/de/kontextwork/converter/module/convert/controller/ConversionController.kt
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -26,6 +27,7 @@ class ConversionController(
     private val fileNameUtils: FileNameUtils
 ) : Logging {
     @PostMapping(path = [""])
+    @CrossOrigin(origins = "*")
     @Throws(IOException::class)
     fun convert(
         @RequestParam(name = "format", defaultValue = "pdf") targetFormatExt: String,
